fa5dd21833845176293466404bb1bea34432ef4e
vadempur <vadempur@gmail.com>
2019-02-16 01:28:59 +0530

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { WavesModule, TableModule,ButtonsModule,CollapseModule } from 'angular-bootstrap-md';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { AppRoutingModule } from "./app-routing.module";
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { AppComponent } from "./app.component";
+import {
+  WavesModule,
+  TableModule,
+  ButtonsModule,
+  CollapseModule
+} from "angular-bootstrap-md";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WavesModule, 
+    WavesModule,
     TableModule,
     ButtonsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
     CollapseModule,
+    BrowserAnimationsModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
